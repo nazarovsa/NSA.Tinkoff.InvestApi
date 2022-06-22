5658a85c7a50600c3e89d1f4c232a4f6daccc3f5
Sergey Nazarov <insight.appdev@gmail.com>
2022-06-22 22:42:41 +0300

    Extend token extraction for testing.

--- tests/NSA.Tinkoff.InvestApi.Testing/AccessTokenAccessor.cs
+++ tests/NSA.Tinkoff.InvestApi.Testing/AccessTokenAccessor.cs
@@ -5,6 +5,13 @@ public static class AccessTokenAccessor
     public static string GetFromEnv()
     {
         var token = Environment.GetEnvironmentVariable("NSA_INVESTAPI_ACCESSTOKEN", EnvironmentVariableTarget.Process);
+        
+        if(string.IsNullOrWhiteSpace(token))
+            token = Environment.GetEnvironmentVariable("NSA_INVESTAPI_ACCESSTOKEN", EnvironmentVariableTarget.User);
+        
+        if(string.IsNullOrWhiteSpace(token))
+            token = Environment.GetEnvironmentVariable("NSA_INVESTAPI_ACCESSTOKEN", EnvironmentVariableTarget.Machine);
+        
         if (string.IsNullOrWhiteSpace(token))
         {
             throw new ArgumentNullException(nameof(token));
